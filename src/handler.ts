2c2ea73bebfecdbec9610cdd6854abb30c006459
mi-upto <misato1989x@gmail.com>
2020-05-21 15:54:44 +0900

    ✨  API response に型定義

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,19 @@ import {
   APIGatewayProxyResult,
 } from "aws-lambda";
 
+import { LeagueResponse } from './types'
+
+// @see https://spla2.yuu26.com/
+//spla2.yuu26.com/league/now
+//spla2.yuu26.com/league/next
+const SPLA2_API = 'https://spla2.yuu26.com/';
+
+
+export async function getSPLATOON_STAGE(type: String, When: String): LeagueResponse {
+
+}
+
+
 export async function handler(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
---
+++ src/types/model.type.ts
@@ -0,0 +1,31 @@
+type Rule =  "ガチヤグラ" | "ガチアサリ" | 'ガチエリア' | 'ガチホコ';
+
+export interface LeagueResponse {
+  result: StageData[]
+}
+
+interface StageData {
+  rule: Rule;
+  rule_ex: StageRuleEx;
+  maps: Array<String>;
+  maps_ex: Array<MapsEx>; // maps_ex: MapsEx[];
+  start: string;
+  start_utc: string;
+  start_t: number;
+  end: string;
+  end_utc: string;
+  end_t: number;
+}
+
+interface StageRuleEx {
+  key: string;
+  name: Rule;
+  statink: string;
+}
+
+interface MapsEx {
+  id: number;
+  name: string;
+  image: string;
+  statink: string;
+}
